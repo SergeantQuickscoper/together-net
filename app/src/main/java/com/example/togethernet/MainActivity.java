6237a141d9135e693bced48c41bf73030879673a
Don Roy Chacko <donisepic30@gmail.com>
2025-10-12 17:31:33 +0530

    feat: implement basic activity transfer b/w first two, and also add perms to manifest

--- app/src/main/java/com/example/togethernet/MainActivity.java
+++ app/src/main/java/com/example/togethernet/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button onboardingButton;
+        onboardingButton = findViewById(R.id.onboardingButton);
+        onboardingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // TODO: check for
+                Intent switchToPermsActivity = new Intent(MainActivity.this, PermissionsActivity.class);
+                startActivity(switchToPermsActivity);
+            }
         });
     }
 }
--- app/src/main/java/com/example/togethernet/PermissionsActivity.java
+++ app/src/main/java/com/example/togethernet/PermissionsActivity.java
@@ -1,14 +1,21 @@
 package com.example.togethernet;
 
 import android.os.Bundle;
-
+import android.widget.Button
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.content.Intent;
 
 public class PermissionsActivity extends AppCompatActivity {
+    Button btPermsBtn;
+    Button wifiPermsBtn;
+    Button fgPermsBtn;
+    Button locationPermsBtn;
+    Button createNickNameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,49 @@ public class PermissionsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btPermsBtn = findViewById(R.id.BTperms);
+        wifiPermsBtn = findViewById(R.id.WIFIperms);
+        fgPermsBtn = findViewById(R.id.ForegroundPerms);
+        locationPermsBtn = findViewById(R.id.LocationPerms);
+        createNickNameBtn = findViewById(R.id.CreateNickNameButton);
+        btPermsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Handle Bluetooth permissions
+
+
+            }
+        });
+
+        wifiPermsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Handle WiFi permissions
+            }
+        });
+
+        fgPermsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Handle Foreground permissions
+            }
+        });
+
+        locationPermsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Handle Location permissions
+            }
+        });
+
+        createNickNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Handle continue button click (e.g., go to next screen)
+                System.out.println("Hello");
+            }
         });
     }
 }
